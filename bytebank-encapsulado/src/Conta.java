a25080ca0704e9e85e2426982190ee427d0ef261
Gustavo Luis dos Santos <gustavobhm@bol.com.br>
2019-04-15 08:35:32 -0300

    Aula 6 - Construtores e membros estáticos

--- bytebank-encapsulado/src/Conta.java
+++ bytebank-encapsulado/src/Conta.java
@@ -5,6 +5,16 @@ public class Conta {
 	private int agencia;
 	private int numero;
 	private Cliente titular;
+	private static int total = 0;
+
+	public Conta(int agencia, int numero) {
+        Conta.total++;
+        System.out.println("O total de contas é " + Conta.total);
+        this.agencia = agencia;
+        this.numero = numero;
+        this.saldo = 100;
+        System.out.println("Estou criando uma conta" + this.numero);
+	}
 
 	public void deposita(double valor) {
 		this.saldo += valor;
@@ -40,10 +50,18 @@ public class Conta {
 	}
 
 	public void setNumero(int numero) {
+		if (numero <= 0) {
+			System.out.println("não pode valor <= 0");
+			return;
+		}
 		this.numero = numero;
 	}
 
 	public int getAgencia() {
+		if (agencia <= 0) {
+			System.out.println("nao pode valor menor igual a 0");
+			// return;
+		}
 		return this.agencia;
 	}
 
@@ -57,6 +75,10 @@ public class Conta {
 
 	public void setTitular(Cliente titular) {
 		this.titular = titular;
+	}
+
+	public static int getTotal() {
+		return Conta.total;
 	}
 
 }
